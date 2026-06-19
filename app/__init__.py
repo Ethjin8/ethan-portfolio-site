a2c519413c6f04336f4d75c8109c773836c42f62
Ethan Jin <ethjin8@gmail.com>
2026-06-19 13:39:41 -0700

    Added navbar routing

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="Ethan Jin", url=os.getenv("URL"))
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About Me")
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html', title="Experience")
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html', title="Hobbies")
+
+@app.route('/map')
+def map_page():
+    return render_template('map.html', title="Map")
